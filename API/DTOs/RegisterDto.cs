523b1b1ac6d71a2465dbef9c5a319183bc59bdb0
Cristutiu Emanuel <cristutiumanu@gmail.com>
2022-12-31 00:26:56 +0200

    Added functionality for Registration.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,17 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
-        
+        [Required]
+        public string KnowsAs { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateOnly? DateOfBirth { get; set; } // I made this optional so I can make require work!
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+
         [Required]
         [StringLength(12, MinimumLength = 4)]
         public string Password { get; set; }
